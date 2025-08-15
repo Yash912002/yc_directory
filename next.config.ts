62da238ded9bfd507f765457dfbea5c5010d3080
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-08-15 20:32:09 +0530

    Remove after: true from experimental

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    dangerouslyAllowSVG: true,
-    remotePatterns : [
-      {
-        protocol: "https",
-        hostname: "*"
-      }
-    ]
-  },
-  experimental: {
-    ppr: "incremental",
-    after: true
-  },
-  devIndicators: {
-    appIsrStatus: true,
-    buildActivity: true,
-    buildActivityPosition: "bottom-right"
-  }
+	images: {
+		dangerouslyAllowSVG: true,
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "*",
+			},
+		],
+	},
+	experimental: {
+		ppr: "incremental",
+	},
+	devIndicators: {
+		appIsrStatus: true,
+		buildActivity: true,
+		buildActivityPosition: "bottom-right",
+	},
 };
 
 export default nextConfig;
