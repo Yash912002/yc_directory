7286e2840a1b71cf7789d9ab92d9486e06201a0f
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-08-15 20:32:58 +0530

    Remove unstable_after import from View component

--- components/View.tsx
+++ components/View.tsx
@@ -2,7 +2,7 @@ import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
 import { STARTUPS_VIEWS_QUERY } from "@/sanity/lib/queries";
-import { unstable_after as after } from "next/server";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
 	// Fetch the total views for a specific startup
